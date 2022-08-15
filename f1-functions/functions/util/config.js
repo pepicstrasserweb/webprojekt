4b74b04e145df391d6b163ff35438cba64485bca
Robert Pepić <r.pepic97@gmail.com>
2022-08-15 19:38:14 +0200

    upload_projekta

---
+++ f1-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyClzHkQq3e1uPFPI4lKQG2nbBJtNVzsXg0",
+    authDomain: "f1-vp-17377.firebaseapp.com",
+    databaseURL:
+      "https://f1-vp-17377-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "f1-vp-17377",
+    storageBucket: "f1-vp-17377.appspot.com",
+    messagingSenderId: "256327583290",
+    appId: "1:256327583290:web:42f7010a25a5e6fc895703",
+  };
